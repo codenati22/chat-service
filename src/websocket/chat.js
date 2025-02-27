fd23ce845517e39dfa25c86344315f11ac093ddb
codenati22 <natnaelgirma077@gmail.com>
2025-02-28 00:42:33 +0300

    accepting username in socket

--- src/websocket/chat.js
+++ src/websocket/chat.js
@@ -43,7 +43,8 @@ wss.on("connection", (ws, req) => {
           if (client.readyState === WebSocket.OPEN) {
             client.send(
               JSON.stringify({
-                user: ws.user.id,
+                userId: ws.user.id,
+                user: ws.user.username,
                 content,
                 timestamp: msg.timestamp,
               })
@@ -67,3 +68,72 @@ wss.on("connection", (ws, req) => {
 });
 
 module.exports = { wss };
+// const WebSocket = require("ws");
+// const Message = require("../models/message");
+
+// const wss = new WebSocket.Server({ noServer: true });
+// const chats = new Map();
+
+// wss.on("connection", (ws, req) => {
+//   const urlParts = req.url.split("?");
+//   const streamId = urlParts[0].split("/")[1];
+//   const query = new URLSearchParams(urlParts[1]);
+//   const token = query.get("token");
+
+//   if (!streamId || !token) {
+//     ws.close();
+//     return;
+//   }
+
+//   try {
+//     const { verifyToken } = require("../utils/auth");
+//     const user = verifyToken(token);
+//     ws.user = user;
+//   } catch (error) {
+//     ws.close();
+//     return;
+//   }
+
+//   if (!chats.has(streamId)) {
+//     chats.set(streamId, new Set());
+//   }
+//   chats.get(streamId).add(ws);
+
+//   ws.on("message", async (message) => {
+//     const data = JSON.parse(message);
+//     const { content } = data;
+
+//     try {
+//       const msg = new Message({ streamId, userId: ws.user.id, content });
+//       await msg.save();
+
+//       const chat = chats.get(streamId);
+//       if (chat) {
+//         chat.forEach((client) => {
+//           if (client.readyState === WebSocket.OPEN) {
+//             client.send(
+//               JSON.stringify({
+//                 user: ws.user.id,
+//                 content,
+//                 timestamp: msg.timestamp,
+//               })
+//             );
+//           }
+//         });
+//       }
+//     } catch (error) {
+//       console.error("Chat message error:", error);
+//     }
+//   });
+
+//   ws.on("close", () => {
+//     const chat = chats.get(streamId);
+//     if (!chat) return;
+//     chat.delete(ws);
+//     if (chat.size === 0) {
+//       chats.delete(streamId);
+//     }
+//   });
+// });
+
+// module.exports = { wss };
